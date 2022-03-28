5bde04523f950f9ded9195efb0c3f5a049491a8b
Cole Henderson <darkgale93@gmail.com>
2022-03-28 12:40:41 -0600

    search works

--- src/components/App.js
+++ src/components/App.js
@@ -12,11 +12,13 @@ class App extends Component {
 
     this.state = {
       posts: [],
+      search: "",
     };
 
     this.updatePost = this.updatePost.bind(this);
     this.deletePost = this.deletePost.bind(this);
     this.createPost = this.createPost.bind(this);
+    this.updateSearch = this.updateSearch.bind(this);
   }
 
   componentDidMount() {
@@ -48,26 +50,41 @@ class App extends Component {
         this.setState({ posts: results.data });
       });
   }
-
+  updateSearch(value) {
+    this.setState({ search: value });
+  }
   render() {
-    const { posts } = this.state;
-
+    const { posts, search } = this.state;
+    const filtered = posts.filter((ele) => {
+      return ele.text.includes(search);
+    });
     return (
       <div className="App__parent">
-        <Header />
+        <Header search={search} updateSearch={this.updateSearch} />
 
         <section className="App__content">
           <Compose createPostFn={this.createPost} />
-          {posts.map((post) => (
-            <Post
-              key={post.id}
-              id={post.id}
-              text={post.text}
-              date={post.date}
-              updatePostFn={this.updatePost}
-              deletePostFn={this.deletePost}
-            />
-          ))}
+          {search.length > 0
+            ? filtered.map((post) => (
+                <Post
+                  key={post.id}
+                  id={post.id}
+                  text={post.text}
+                  date={post.date}
+                  updatePostFn={this.updatePost}
+                  deletePostFn={this.deletePost}
+                />
+              ))
+            : posts.map((post) => (
+                <Post
+                  key={post.id}
+                  id={post.id}
+                  text={post.text}
+                  date={post.date}
+                  updatePostFn={this.updatePost}
+                  deletePostFn={this.deletePost}
+                />
+              ))}
         </section>
       </div>
     );
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
+// @ts-nocheck
+import React, { Component } from "react";
 
-import './Search.css';
+import "./Search.css";
 
-import SearchIcon from 'react-icons/lib/md/search';
+import SearchIcon from "react-icons/lib/md/search";
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
   render() {
+    const { searchText, updateSearch } = this.props;
+    const onChangeHandle = (ev) => updateSearch(ev.target.value);
     return (
       <section className="Search__parent">
-
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input
+            onChange={onChangeHandle}
+            value={searchText}
+            placeholder="Search Your Feed"
+          />
 
           <SearchIcon id="Search__icon" />
         </div>
-        
       </section>
-    )
+    );
   }
 }
